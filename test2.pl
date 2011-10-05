c6eacf895a8b27e83f4ca8a69c989058137849f3
Alexandra <hertealexandra@yahoo.com>
2011-10-05 17:20:53 +0300

    mesaj

---
+++ test2.pl
@@ -0,0 +1,31 @@
+#$a=<STDIN>;
+$b="ceva";
+$c=3.4;
+#print "a=$a,b=$b,c=$c";
+#print 'a=$a,b=$b,c=$c';
+#$a="mesaj";
+#print $a;
+#@vector=(1,2,3,4);
+#print $vector[0];
+#print $vector[-1];
+#$a=scalar@vector;
+#print $a;
+=pod
+%zile=(		
+	"Luni"=>1,
+	"Marti"=>2,
+	"Miercuri"=>3
+	);
+print $zile {"Marti"};
+print keys %zile;
+print values %zile;
+=cut
+$ a=4;
+if ($a==3)
+{
+  print "a=3";
+}
+else
+{
+	print "a=4";
+}
